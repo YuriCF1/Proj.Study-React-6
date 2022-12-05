17fc0cb8aa1eda3c0596a5c98a229d28a74c9b9e
Yuri <yurifdev@gmail.com>
2022-12-05 10:27:07 -0300

    Creating layout of the main page of the game

--- secret-word/src/components/Game.js
+++ secret-word/src/components/Game.js
@@ -1,30 +1,30 @@
-import {style} from './Game.module.css'
+import style from './Game.module.css'
 
 import React from 'react'
 
 const Game = ({verifyLetter}) => {
   return (
 
-    <div className="game">
-      <p className="points">
+    <div className={style.game}>
+      <p className={style.points}>
         <span>Pontuação: 000</span>
       </p>
       <h1>Advinhe a palavra</h1>
-      <h3 className="type">
+      <h3 className={style.tip}>
         Dica sobre a palavra: <span>Dica...</span>
       </h3>
-      <div className="wordContainer">
-        <span className='letter'>A</span>
-        <span className='blankSquare'>B</span>
+      <div className={style.wordContainer}>
+        <span className={style.letter}>A</span>
+        <span className={style.blankSquare}>B</span>
       </div>
-      <div className="letterContainer">
+      <div className={style.letterContainer}>
         <p>Tente advinha a letra da palavra</p>
         <form action="">
           <input type="text" name='letter' maxLength="1" required/>
           <button>Jogar </button>
         </form>
       </div>
-      <div className="wrongLettersContainer">
+      <div className={style.wrongLettersContainer}>
         <p>Letras já utilizadas</p>
         <span>b, c</span>
         <span>d, e</span>
